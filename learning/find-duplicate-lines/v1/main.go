84309acf9ab32d101e3b862be2c6628f3914a300
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2020-04-25 18:01:43 -0300

    feat: Add  `find-duplicate-lines@v1`

---
+++ learning/find-duplicate-lines/v1/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		if isTerm() && text == ".exit" {
+			break
+		}
+		counts[text]++
+	}
+
+	fmt.Println("==== Count Result ====")
+	for line, count := range counts {
+		fmt.Printf("%d - %s\n", count, line)
+	}
+}
+
+func isTerm() bool {
+	stats, err := os.Stdin.Stat()
+	if err != nil {
+		panic(err)
+	}
+	return ((stats.Mode() & os.ModeCharDevice) == os.ModeCharDevice)
+}
